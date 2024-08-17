c52bd072d35d3c20e1f88c52a56588b01be1c35b
tomit4 <mosssap@gmail.com>
2024-08-17 13:00:14 -0700

    :wrench: Adjusted env config and start script

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+source ./.env
+
 if [ ! -d "./env" ]; then
     python3 -m venv env
 fi
 
 source ./env/bin/activate &&
     # python ./main.py
-    fastapi dev ./main.py
+    fastapi dev ./main.py --host "${HOST}" --port "${PORT}"
